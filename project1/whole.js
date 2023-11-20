2430da4bcdfd2368cdf9b13db1c94c14f55f87da
dysong08 <151178656+dysong08@users.noreply.github.com>
2023-11-21 06:18:42 +0900

    feat:2023_11_21
    수정본... 거의마지막

--- project1/whole.js
+++ project1/whole.js
@@ -1,12 +1,18 @@
+
+// 메인메뉴바에 hover시 하위메뉴바 노출시키기
+
 var currHover = "";
+// currHover : hover 존재 여부 체크할 변수
 
 $(".navbar").hover(
     (e) => {
         if (currHover != "") {
-            $(`#${currHover}`).hide()
+            $(`#${currHover}`).hide();
+            // hover가 있으면 기존 hover는 숨기기
         }
 
         var className = $(e.target).attr('class');
+        // 이벤트가 발생한 요소의 클래스를 가져와서 변수 className에 저장
         if (className) {
             if (className === "navbar men-menu") {
                 currHover = "men-menu"
@@ -20,13 +26,15 @@ $(".navbar").hover(
     },
     () => {
         $(`#${currHover}`).hover(
-            () => { /* 하단 메뉴 바 안쪽에서 놀고 있을때 */ },
+            () => { /* 하단 메뉴바 안쪽에 커서가 있을때 */ },
             () => { $(`#${currHover}`).hide(); }
-
+            // 아니면 숨기기
         );
     }
 );
 
+
+// hover로 img 전환시키기
 $(".changeImg-main").hover(
     (e) => {
         e.target.src = "img/2_right2_img.png";
@@ -37,12 +45,15 @@ $(".changeImg-main").hover(
 );
 
 
+// main -big-img 전환시키기
 const imgList = ["main_img1", "main_img2", "main_img3", "main_img4"];
-let currImgIndex = 0;
-let prevImgIndex = 0;
-document.querySelector(`.count${currImgIndex}`).style.backgroundColor = "black";
+let currImgIndex = 0;   // 오른쪽으로 전환시 개수 count
+let prevImgIndex = 0;   // 왼쪽으로 전환시 개수 count
 
-const plusImg = () => {
+document.querySelector(`.count${currImgIndex}`).style.backgroundColor = "black";
+// .img-count-container안의 div4개를 변경될때마다 배경색 black으로 변경시키기
+
+const nextImg = () => {
     prevImgIndex = currImgIndex;
     if (currImgIndex < imgList.length - 1)
         currImgIndex += 1;
@@ -53,7 +64,7 @@ const plusImg = () => {
     changed_img_selector();
 }
 
-const minusImg = () => {
+const prevImg = () => {
     prevImgIndex = currImgIndex;
     if (currImgIndex > 0)
         currImgIndex -= 1;
@@ -64,9 +75,14 @@ const minusImg = () => {
     changed_img_selector();
 }
 
+function changed_img_selector() {
+    document.querySelector(`.count${prevImgIndex}`).style.backgroundColor = "rgb(149, 156, 255)";
+    document.querySelector(`.count${currImgIndex}`).style.backgroundColor = "black";
+
+}
 
 setInterval(() => {
-    plusImg();
+    nextImg();
 }, 5000)
 
 
@@ -74,19 +90,13 @@ const stopInterval = () => { clearInterval(interval); };
 
 document.getElementById("arrowBtn-left").addEventListener('click',
     () => {
-        minusImg();
+        prevImg();
     }
 );
 document.getElementById("arrowBtn-right").addEventListener('click',
     () => {
-        plusImg();
+        nextImg();
     }
 );
-$("#arrowBtn-left").hover(() => { $(this).css("cursor", "pointer") });
 
 
-function changed_img_selector() {
-    document.querySelector(`.count${prevImgIndex}`).style.backgroundColor = "rgb(149, 156, 255)";
-    document.querySelector(`.count${currImgIndex}`).style.backgroundColor = "black";
-
-}
